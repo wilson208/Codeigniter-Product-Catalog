f91e1c218b46b70fdc13c3278ac820a9d2785cf7
Wilson McCoubrey <wilson@mccoubreys.co.uk>
2014-03-08 20:19:33 +0000

    Start made on admin section. Header, footer, login. Ajax email validation sorted on user registration.

--- store_app/views/common/header.php
+++ store_app/views/common/header.php
@@ -24,7 +24,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-40701782-2', {'cookieDomain': 'none'});
+  ga('create', 'UA-40701782-2', 'none');
   ga('send', 'pageview');
 
 </script>
