4e440a966842d9227cfb7f304d3e59441693fb09
Asus <Asus@10.37.31.8>
2014-04-04 01:17:10 +0100

    Merge origin/master

--- store_app/views/product/review.php
+++ store_app/views/product/review.php
@@ -3,6 +3,6 @@
     <div class="col-lg-5"><?php echo $review->name; ?></div>
     <div class="right"><?php echo date('jS M Y', strtotime($review->date)); ?></div>
     <div ><?php echo $review->score; ?></div>
-    <div class="star"
+   
     <div class="col-lg-7"><?php echo $review->review; ?></div>
 </div>
